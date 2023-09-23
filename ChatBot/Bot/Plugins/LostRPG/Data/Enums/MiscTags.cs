a88f2fe0c9d7bf3b264012b1457287653b68f2b8
Sense <nall_aeternus@hotmail.com>
2023-09-22 20:18:09 -0400

    More dumb changes

---
+++ ChatBot/Bot/Plugins/LostRPG/Data/Enums/MiscTags.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+
+namespace ChatBot.Bot.Plugins.LostRPG.Data.Enums
+{
+    public enum RequiredSkillTags
+    {
+        tags,
+        level,
+        effects,
+        speed,
+        name,
+    }
+
+    public enum OptionalSkillTags
+    {
+        reaction,
+        description,
+    }
+
+    public enum EffectTypes
+    {
+        [Description("b")]
+        Buff,
+        [Description("db")]
+        Debuff,
+    }
+}
